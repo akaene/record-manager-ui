470ad6ff705e8a331e3b8c784b0af13eab5492bd
shellyear <boolevafunkce@gmail.com>
2024-02-27 10:55:58 +0100

    [feature] migrate webpack to vite;
    remove everything webpack related
    add vite config
    format code and docker-compose to be able to work with vite
    add env variables for dev and prod
    remove port env varibles from .env.development and .env.production
    install and use bootstrap properly
    remove webpack-related package
    remove unnecessary vite ts package

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {IntlProvider} from "react-intl";
 import {Route, Router, Switch} from "react-router-dom";
 import MainView from "./components/MainView";
 import {connect} from "react-redux";
 import {history} from "./utils/Routing";
-import {BASENAME} from "../config";
+import {APP_TITLE, BASENAME} from "../config";
 import OidcAuthWrapper from "./components/misc/oidc/OidcAuthWrapper";
 import OidcSignInCallback from "./components/pages/OidcSignInCallback";
 import OidcSilentCallback from "./components/pages/OidcSilentCallback";
 import {isUsingOidcAuth} from "./utils/OidcUtils";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/record-manager.css"
 import "@kbss-cvut/s-forms/css"
 
 const App = (props) => {
+    useEffect(() => {
+        document.title = APP_TITLE;
+    }, [APP_TITLE]);
+    
     return <IntlProvider {...props.intl}>
         <Router history={history} basename={BASENAME}>
             <Switch>
